d9fe701da5605dadc3dc3dc0e111a65a681f09ee
Andrew <andrew.j.heinke@gmail.com>
2016-12-25 23:25:20 -0600

    adding main js file for update put requests

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('./public'));
 
+app.use(bodyParser.json())
+
 app.get('/', (req, res) => {
   db.collection('quotes').find().toArray((err, result) => {
     if (err) return console.log(err);
@@ -31,5 +33,19 @@ app.post('/quotes', (req, res) => {
     if (err) return console.log(err);
     console.log('saved to database');
     res.redirect('/');
+  });
+});
+
+app.put('/quotes', (req, res) => {
+  db.collection('quotes').findOneAndUpdate({name: 'Yoda'}, {$set: {
+    name: req.body.name,
+    quote: req.body.quote
+  }
+  }, {
+    sort: {_id: -1},
+    upsert: true
+  }, (err, result) => {
+    if (err) return res.send(err);
+    res.send(result);
   });
 });
